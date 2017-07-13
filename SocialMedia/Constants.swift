a9e258a0bfc1cd2261df3522a692af9a5fa35841
Goldshawn <shalom.shawn@yahoo.com>
2017-07-13 09:17:59 +0100

    tweeked the UI
    nothing major

---
+++ SocialMedia/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SocialMedia
+//
+//  Created by Shalom Owolabi on 13/07/2017.
+//  Copyright © 2017 Learning. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120/255
---
+++ SocialMedia/roundedButton.swift
@@ -0,0 +1,31 @@
+//
+//  roundedButton.swift
+//  SocialMedia
+//
+//  Created by Shalom Owolabi on 13/07/2017.
+//  Copyright © 2017 Learning. All rights reserved.
+//
+
+import UIKit
+
+class roundedButton: UIButton {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        
+        imageView?.contentMode = .scaleAspectFit
+        
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = self.frame.height / 2
+    }
+
+}
